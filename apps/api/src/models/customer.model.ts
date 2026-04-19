e40de56c5a63e5028d15c949476acac4f349630a
aseelnour <165104559+aseelnour@users.noreply.github.com>
2026-04-19 17:52:16 +0300

    create customer model schema

---
+++ apps/api/src/models/customer.model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+import { Gender } from "../enums/customer-gender.enum";
+import { ICustomer } from "../interfaces/Customer.interface";
+
+const CustomerSchema = new Schema<ICustomer>(
+  {
+    email: { type: String, required: true },
+    password: { type: String, required: true }, 
+    fullName: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    gender: {
+      type: String,
+      enum: Object.values(Gender),
+      required: false,
+    },
+    dob: { type: Date, required: false },
+    city: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<ICustomer>("Customer", CustomerSchema);
