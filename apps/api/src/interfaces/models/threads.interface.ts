c915da855468027dbb5276fd101217dd1175d8e3
BAhaa Enab <bahaenab@gmail.com>
2026-04-20 07:28:17 +0300

    EP-9-api-Threads-edit-2

--- apps/api/src/interfaces/models/threads.interface.ts
+++ apps/api/src/interfaces/models/threads.interface.ts
@@ -1,4 +1,5 @@
+import { Schema, model, Types } from "mongoose";
 export interface IThread {
-  senderId: string;
-  receiverId?: string; 
+  senderId: Types.ObjectId;
+  receiverId?: Types.ObjectId; 
 }
--- apps/api/src/models/thread.ts
+++ apps/api/src/models/thread.ts
@@ -1,9 +1,9 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 import { IThread } from "../interfaces/models/threads.interface";
 
 const threadSchema = new Schema<IThread>({
-    senderId: { type: String, required: true , ref: "Customer" },
-    receiverId: { type: String, required: false , ref: "Client" },
+senderId: { type: Schema.Types.ObjectId, ref: "Customer" },
+receiverId: { type: Schema.Types.ObjectId, ref: "Client" },
 }, { collection: "threads" });
 
 export const Thread = model("Thread", threadSchema);
