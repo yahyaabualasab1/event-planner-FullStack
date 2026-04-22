c845a19a7ef7890037a8adbe4d71a05406187f54
yahyaabualasab1 <s12217938@stu.najah.edu>
2026-04-22 17:17:39 +0300

    Refactor booking service methods

--- apps/api/src/services/admin-system/bookings/booking.services.ts
+++ apps/api/src/services/admin-system/bookings/booking.services.ts
@@ -2,31 +2,31 @@ import { Booking} from "../../../models/booking";
 import { IBooking } from "../../../interfaces/models/booking.interface";
 import { BookingEnum } from "../../../enums/models/booking";
 export const getAllBookings = async (data : BookingEnum ) => {
-    return await Booking.find({ deletedAt: null });
+    return await Booking.find();
 };
 export const getBookingById = async (id: string) => {
-    return await Booking.findOne({ _id: id, deletedAt: null });
+    return await Booking.findById(id);
 };
 export const getBookingsByClientId = async (clientId: number) => {
-    return await Booking.find({ clientId: clientId, deletedAt: null });
+    return await Booking.find({ clientId: clientId });
 };
 
 export const getBookingsByVenueId = async (venueId: number) => {
-    return await Booking.find({ venueId: venueId, deletedAt: null });
+    return await Booking.find({ venueId: venueId });
 };
 export const getBookingsByCustomerId = async (customerId: number) => {
-    return await Booking.find({ customerId: customerId, deletedAt: null });
+    return await Booking.find({ customerId: customerId });
 };
 export const getBookingsByStatus = async (status: BookingEnum) => {
-    return await Booking.find({ status: status, deletedAt: null });
+    return await Booking.find({ status: status });
 };
 export const createBooking = async (bookingData: IBooking) => {
     return await Booking.create(bookingData);
 };
 export const updateBooking = async (id: string, bookingData: Partial<IBooking>) => {
-    return await Booking.findOneAndUpdate({ _id: id, deletedAt: null }, bookingData, { new: true });
+    return await Booking.findByIdAndUpdate(id, bookingData, { new: true });
 };
 export const deleteBooking = async (id: string) => {
-    return await Booking.findOneAndUpdate({ _id: id, deletedAt: null }, { deletedAt: new Date() }, { new: true });
+    return await Booking.findByIdAndDelete(id);
 };
 
