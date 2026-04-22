fcf97739af8d0a2ea9807ac69abec6a0574e32cb
Shahd <shahdebad@gmail.com>
2026-04-22 22:55:56 +0300

    feat: add soft delete to venues ep-16

--- apps/api/src/services/admin-system/admin/venues.services.ts
+++ apps/api/src/services/admin-system/admin/venues.services.ts
@@ -1,46 +1,54 @@
 import { Venue } from "../../../models/venue.model";
 import type {
-	CreateVenueInput,
-	UpdateVenueInput,
+  CreateVenueInput,
+  UpdateVenueInput,
 } from "../../../validation/admin-system/venues.schemas";
 
 export async function getAllVenues() {
-	return await Venue.find();
+  return await Venue.find({ isDeleted: false });
 }
 
 export async function getVenueById(id: string) {
-	const venue = await Venue.findById(id);
-	if (!venue) {
-		throw new Error("Venue not found");
-	}
-	return venue;
+  const venue = await Venue.findOne({ _id: id, isDeleted: false });
+  if (!venue) {
+    throw new Error("Venue not found");
+  }
+  return venue;
 }
 
 export async function getVenuesByClientId(clientId: string) {
-	const venues = await Venue.find({ clientId });
-	if (!venues.length) {
-		throw new Error("No venues found for this client");
-	}
-	return venues;
+  const venues = await Venue.find({ clientId, isDeleted: false });
+  if (!venues.length) {
+    throw new Error("No venues found for this client");
+  }
+  return venues;
 }
 
 export async function createVenue(data: CreateVenueInput) {
-	const venue = new Venue(data);
-	return await venue.save();
+  const venue = new Venue(data);
+  return await venue.save();
 }
 
 export async function updateVenue(id: string, data: UpdateVenueInput) {
-	const venue = await Venue.findByIdAndUpdate(id, data, { new: true });
-	if (!venue) {
-		throw new Error("Venue not found");
-	}
-	return venue;
+  const venue = await Venue.findOneAndUpdate(
+    { _id: id, isDeleted: false },
+    data,
+    { new: true }
+  );
+  if (!venue) {
+    throw new Error("Venue not found");
+  }
+  return venue;
 }
 
 export async function deleteVenue(id: string) {
-	const venue = await Venue.findByIdAndDelete(id);
-	if (!venue) {
-		throw new Error("Venue not found");
-	}
-	return venue;
+  const venue = await Venue.findOneAndUpdate(
+    { _id: id, isDeleted: false },
+    { isDeleted: true },
+    { new: true }
+  );
+  if (!venue) {
+    throw new Error("Venue not found");
+  }
+  return venue;
 }
