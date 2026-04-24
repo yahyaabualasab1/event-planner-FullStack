1575f7c3c2e3c7e06f1dd2e44a5bda72c7ca71e3
faraj <function.faraj@gmail.com>
2026-04-24 14:11:37 +0300

    Remove client profile routes and update import path in client index file

--- apps/api/src/routes/admin-system/client/index.ts
+++ apps/api/src/routes/admin-system/client/index.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { adminAuthRoutes } from "../admin/auth.routes";
-import { clientProfileRoutes } from "./clientProfile.routes";
+import { clientProfileRoutes } from "./client-profile.routes";
 
 const router = Router();
 
