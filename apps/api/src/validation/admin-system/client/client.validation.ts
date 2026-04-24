b7023c6764abd3ebbd54b2d732387041db3ef193
aseelnour <165104559+aseelnour@users.noreply.github.com>
2026-04-24 13:52:23 +0300

    Refactor client validation to use Types from mongoose

--- apps/api/src/validation/admin-system/client/client.validation.ts
+++ apps/api/src/validation/admin-system/client/client.validation.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import mongoose from "mongoose";
+import { Types } from "mongoose";
 import { ClientStatusEnum } from "../../../enums/models/client.status";
 
 export const createClientSchema = z.object({
@@ -7,15 +7,17 @@ export const createClientSchema = z.object({
   password: z.string().min(6),
   fullName: z.string(),
   phoneNumber: z.string(),
+  status: z
+    .nativeEnum(ClientStatusEnum)
+    .default(ClientStatusEnum.WAITING_APPROVE),
 });
 
 export const updateStatusSchema = z.object({
   status: z.nativeEnum(ClientStatusEnum),
 });
 
-
 export const idParamSchema = z.object({
-  id: z.string().refine((val) => mongoose.Types.ObjectId.isValid(val), {
+  id: z.string().refine((val) => Types.ObjectId.isValid(val), {
     message: "Invalid ID format",
   }),
 });
