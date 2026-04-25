0b59c98163d2f581f32e48beaa3e952cd4f01f33
faraj <function.faraj@gmail.com>
2026-04-25 13:19:28 +0300

    Remove all route definitions from the API index file, effectively clearing the router setup.

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,18 +1,1 @@
-import { Router } from "express";
-
-import { adminAuthRoutes } from "./admin-system/admin/auth.routes";
-import { adminCustomerRoutes } from "./admin-system/customer/customer.routes";
-import { adminVenueRoutes } from "./admin-system/admin/venues.routes";
-import { bookingRoutes } from "./admin-system/bookings/booking.route";
-import { adminMessageRoutes } from "./admin-system/message/message.routes";
-import { adminThreadRoutes } from "./admin-system/thread/thread.routes";
-const router = Router();
-
-router.use("/auth", adminAuthRoutes);
-router.use("/venues", adminVenueRoutes);
-router.use("/customers", adminCustomerRoutes);
-router.use("/bookings", bookingRoutes);
-router.use("/messages", adminMessageRoutes);
-router.use("/threads", adminThreadRoutes);
-
-export { router as apiRoutes };
+export {};
