73b922a7e4704ea8f34bf6b62dfd8427b4af8e0a
Loay Odeh <loayodeh390@gmail.com>
2026-05-16 03:45:16 +0300

    feat: add manage-messages route and translations

--- apps/api/src/routes/client-system/index.ts
+++ apps/api/src/routes/client-system/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-
 import { clientAuthRoutes } from "./client/auth.routes";
+import { clientManageMessagesRoutes } from "./client/manage-messages.routes";
 
 const router = Router();
 
 router.use("/client", clientAuthRoutes);
+router.use("/client/manage-messages", clientManageMessagesRoutes);
 
 export const clientSystemRoutes = router;
