7df470a3ffe103f83f4e2df8ed23c5626b993fb9
faraj <function.faraj@gmail.com>
2026-05-19 17:43:16 +0300

    chore: update Dockerfile.customer to remove VITE_API_URL argument and set axios baseURL to a production endpoint

--- apps/customer/src/services/axios.ts
+++ apps/customer/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-	baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000",
+	baseURL:  "https://dev-event-planner-608310769455.europe-west1.run.app",
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
