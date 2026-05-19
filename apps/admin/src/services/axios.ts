947940f1fcfd5fc6588308c66bc10dff3447d51a
faraj <function.faraj@gmail.com>
2026-05-19 17:24:36 +0300

    chore: update .gitignore to specify environment variable file exclusions and log VITE_API_URL in axios service

--- apps/admin/src/services/axios.ts
+++ apps/admin/src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
-
+console.log(import.meta.env.VITE_API_URL);
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ?? "https://dev-event-planner-608310769455.europe-west1.run.app",
 });
