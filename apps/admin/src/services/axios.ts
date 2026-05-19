9aa41f0d802258b438dedb743ef93adf27d40d92
faraj <function.faraj@gmail.com>
2026-05-19 17:19:54 +0300

    chore: update .dockerignore to refine exclusions and adjust tsconfig settings; add VITE_API_URL support in axios service

--- apps/admin/src/services/axios.ts
+++ apps/admin/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL ?? "https://dev-event-planner-608310769455.europe-west1.run.app",
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
